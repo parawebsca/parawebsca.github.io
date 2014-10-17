e697e3f7de237f6d6f996125d8e81a3e0fc2ab04
gerswin <gerswin@parawebs.com>
2014-10-17 16:19:47 -0430

    fix _blank

--- parawebs.js
+++ parawebs.js
@@ -1,5 +1,6 @@
 var a=document.createElement("a");
 a.setAttribute('href','http://expotachira.net');
+a.setAttribute('target','_blank');
 var pixel=document.createElement("img");
 pixel.setAttribute('src', 'http://ads.parawebs.com/expo2015.jpg');
 pixel.setAttribute('alt', 'holabb');
