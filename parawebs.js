8f5f6a22e3e9816e4b66f4723579276884c2f314
gerswin <gerswin@parawebs.com>
2014-10-17 16:21:00 -0430

    expotachira alt

--- parawebs.js
+++ parawebs.js
@@ -1,8 +1,8 @@
 var a=document.createElement("a");
 a.setAttribute('href','http://expotachira.net');
-a.setAttribute('target','_blank');
+a.setAttribute('target','_blanks');
 var pixel=document.createElement("img");
 pixel.setAttribute('src', 'http://ads.parawebs.com/expo2015.jpg');
-pixel.setAttribute('alt', 'holabb');
+pixel.setAttribute('alt', 'ExpoTachira 2015');
 a.appendChild(pixel);
 document.getElementById('parawebs').appendChild(a);
