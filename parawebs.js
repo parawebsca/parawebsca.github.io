1e3e1a9ace5a97aad19a80dc1605d6ac549af08a
gerswin <gerswin@parawebs.com>
2014-10-17 12:58:57 -0430

    newjs

---
+++ parawebs.js
@@ -0,0 +1,4 @@
+var pixel=document.createElement("img");
+pixel.setAttribute('src', 'http://coderanch.com/mooseImages/moosefly.gif');
+pixel.setAttribute('alt', 'holabb');
+document.getElementById('parawebs').appendChild(pixel);
