0ccc53030713ab98fcca24b69972b577dfd3af8b
gerswin <gerswin@parawebs.com>
2014-10-17 18:26:59 -0430

    fbpixel

--- parawebs.js
+++ parawebs.js
@@ -1,8 +1,31 @@
-var a=document.createElement("a");
-a.setAttribute('href','http://expotachira.net');
-a.setAttribute('target','_blanks');
+(function() {
+ var _fbq = window._fbq || (window._fbq = []);
+ if (!_fbq.loaded) {
+   var fbds = document.createElement('script');
+   fbds.async = true;
+   fbds.src = '//connect.facebook.net/en_US/fbds.js';
+   var s = document.getElementsByTagName('script')[0];
+   s.parentNode.insertBefore(fbds, s);
+   _fbq.loaded = true;
+ }
+})();
+window._fbq = window._fbq || [];
+window._fbq.push(['track', '6018450386021', {'value':'0.00','currency':'USD'}]);
+var noscript =document.createElement("noscript");
 var pixel=document.createElement("img");
-pixel.setAttribute('src', 'http://ads.parawebs.com/expo2015.jpg');
-pixel.setAttribute('alt', 'ExpoTachira 2015');
-a.appendChild(pixel);
-document.getElementById('parawebs').appendChild(a);
+pixel.setAttribute('src', 'https://www.facebook.com/tr?ev=6018450386021&amp;cd[value]=0.00&amp;cd[currency]=USD&amp;noscript=1');
+pixel.setAttribute('alt', 'na');
+pixel.setAttribute('height', '1px');
+pixel.setAttribute('width', '1px');
+pixel.setAttribute('style', 'display:none');
+noscript.appendChild(pixel);
+document.getElementById('parawebs').appendChild(noscript);
+
+var fa=document.createElement("a");
+fa.setAttribute('href','http://expotachira.net');
+fa.setAttribute('target','_blanks');
+var fpixel=document.createElement("img");
+fpixel.setAttribute('src', 'http://ads.parawebs.com/expo2015.jpg');
+fpixel.setAttribute('alt', 'ExpoTachira 2015');
+fa.appendChild(fpixel);
+document.getElementById('parawebs').appendChild(fa);
