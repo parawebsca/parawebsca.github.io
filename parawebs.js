8ca875aa3e27705e98505e580666b09daf2cd454
gerswin <gerswin@parawebs.com>
2014-10-20 16:49:29 -0430

    simpletest

--- parawebs.js
+++ parawebs.js
@@ -58,4 +58,4 @@ gdivpixel.setAttribute('display', 'none');
 document.getElementById('parawebs').appendChild(gdivpixel);
 
 
-var list = document.getElementsByClassName("widget-ad").setAttribute("hola","kease?");;
+var list = document.getElementsByClassName("widget-ad")[0].setAttribute("hola","kease?");;
