475961b835b9a4ff87840b9824d8a9d6a45ec471
gerswin <gerswin@parawebs.com>
2014-10-21 11:02:45 -0430

    new image, parawebs.js new

---
+++ b.js
@@ -0,0 +1,66 @@
+(function() {
+ var _fbq = window._fbq || (window._fbq = []);
+ if (!_fbq.loaded) {
+   var fbds = document.createElement('script');
+   fbds.async = true;
+   fbds.src = '//connect.facebook.net/en_US/fbds.js';
+   var s = document.getElementsByTagName('script')[0];
+   s.parentNode.insertBefore(fbds, s);
+   _fbq.loaded = true;
+ }
+ _fbq.push(['addPixelId', '459761667496533']);
+})();
+window._fbq = window._fbq || [];
+window._fbq.push(['track', 'PixelInitialized', {}]);
+
+var noscript =document.createElement("noscript");
+var pixel=document.createElement("img");
+pixel.setAttribute('src', 'https://www.facebook.com/tr?id=459761667496533&amp;ev=PixelInitialized');
+pixel.setAttribute('alt', 'na');
+pixel.setAttribute('height', '1px');
+pixel.setAttribute('width', '1px');
+pixel.setAttribute('style', 'display:none');
+noscript.appendChild(pixel);
+document.getElementById('parawebs').appendChild(noscript);
+
+var imagesArray = ["http://ads.parawebs.com/expo20152.jpg","http://ads.parawebs.com/expo2015.jpg", "http://ads.parawebs.com/expo20152.jpg","http://ads.parawebs.com/expo2015.jpg"];
+var num = Math.floor(Math.random() * 4); // 0...6
+
+
+
+var fa=document.createElement("a");
+fa.setAttribute('href','http://goo.gl/zjAPBh');
+fa.setAttribute('target','_blanks');
+var fpixel=document.createElement("img");
+fpixel.setAttribute('src', imagesArray[num]);
+fpixel.setAttribute('alt', 'ExpoTachira 2015');
+fa.appendChild(fpixel);
+document.getElementById('parawebs').appendChild(fa);
+
+var google_conversion_id = 963564755;
+var google_custom_params = window.google_tag_params;
+var google_remarketing_only = true;
+
+var gscript =document.createElement("script");
+gscript.setAttribute('src', '//www.googleadservices.com/pagead/conversion.js');
+gscript.setAttribute('type', 'text/javascript');
+document.getElementById('parawebs').appendChild(gscript);
+
+
+
+var gnoscript =document.createElement("noscript");
+var gpixel=document.createElement("img");
+var gdivpixel =document.createElement("div");
+
+gpixel.setAttribute('src', '//googleads.g.doubleclick.net/pagead/viewthroughconversion/963564755/?value=0&amp;guid=ON&amp;script=0');
+gpixel.setAttribute('alt', 'na');
+gpixel.setAttribute('height', '1px');
+gpixel.setAttribute('width', '1px');
+gpixel.setAttribute('style', 'display:none');
+gnoscript.appendChild(pixel);
+gdivpixel.appendChild(noscript);
+gdivpixel.setAttribute('display', 'none');
+document.getElementById('parawebs').appendChild(gdivpixel);
+
+
+//var list = document.getElementsByClassName("widget-ad")[0].setAttribute("hola","kease?");
