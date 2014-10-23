805133e9e292564b6984fea2f1d0e6c6b6740fa9
gerswin <gerswin@parawebs.com>
2014-10-22 20:57:14 -0430

    test

--- parawebs.js
+++ parawebs.js
@@ -67,13 +67,13 @@ google_ad_client = "ca-pub-1109863368642493";
 google_ad_slot = "6117690412";
 google_ad_width = 300;
 google_ad_height = 250;
-document.getElementsByClassName("widget-ad")[0].setAttribute("style","height:250px;width:300px");
-document.getElementsByClassName("widget-ad")[0].innerHTML = ""; 
+document.getElementsByClassName("alignleft")[0].setAttribute("style","height:250px;width:300px");
+document.getElementsByClassName("alignleft")[0].innerHTML = ""; 
 var script = document.createElement("script");
 script.type = "text/javascript";
 script.src = "//pagead2.googlesyndication.com/pagead/show_ads.js";
 if(!document._write) document._write = document.write;
 document.write = function (str) {
-    document.getElementsByClassName("widget-ad")[0].innerHTML += str;
+    document.getElementsByClassName("alignleft")[0].innerHTML += str;
 };
-document.getElementsByClassName("widget-ad")[0].appendChild(script);
+document.getElementsByClassName("alignleft")[0].appendChild(script);
