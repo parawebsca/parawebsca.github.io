94d2dc3a4b449415f6161a814a42e2d8abf89feb
parawebsca <proyectos@parawebs.com>
2015-04-09 16:29:48 -0430

    Updated parawebs.js

--- parawebs.js
+++ parawebs.js
@@ -8,16 +8,12 @@ ga('send', 'pageview');
 
 
 
-window.__insp = window.__insp || [];
-__insp.push(['wid', 1080301835]);
+
 (function() {
-function __ldinsp(){var insp = document.createElement('script'); insp.type = 'text/javascript'; insp.async = true; insp.id = "inspsync"; insp.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://cdn.inspectlet.com/inspectlet.js'; var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(insp, x); }
-if (window.attachEvent) window.attachEvent('onload', __ldinsp);
-else window.addEventListener('load', __ldinsp, false);
+var hm = document.createElement('script'); hm.type ='text/javascript'; hm.async = true;
+hm.src = ('++u-heatmap-it+log-js').replace(/[+]/g,'/').replace(/-/g,'.');
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(hm, s);
 })();
-
-
-
 
 
 document.addEventListener("DOMContentLoaded", function(event) { 
