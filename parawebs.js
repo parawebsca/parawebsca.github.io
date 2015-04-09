17ea50b6702b9ca3e65fd8aefd537902f755a24d
parawebsca <proyectos@parawebs.com>
2015-04-09 19:19:07 -0430

    Updated parawebs.js

--- parawebs.js
+++ parawebs.js
@@ -8,14 +8,6 @@ ga('send', 'pageview');
 
 
 
-
-(function() {
-var hm = document.createElement('script'); hm.type ='text/javascript'; hm.async = true;
-hm.src = ('++u-heatmap-it+log-js').replace(/[+]/g,'/').replace(/-/g,'.');
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(hm, s);
-})();
-
-
 document.addEventListener("DOMContentLoaded", function(event) { 
 var head = document.getElementsByTagName('head')[0],
 script = document.createElement('script');
