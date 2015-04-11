20532e8c1c2c3f1cedba25fa80868ed8545436e5
parawebsca <proyectos@parawebs.com>
2015-04-11 11:01:44 -0430

    Updated parawebs.js

--- parawebs.js
+++ parawebs.js
@@ -6,11 +6,8 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-61679308-1', 'auto');
 ga('send', 'pageview');
 
-
-
-
-document.addEventListener("DOMContentLoaded", function(event) { 
-  console.log("active");
+(function() {
+ console.log("active");
     google_ad_client = "ca-pub-1109863368642493";
     oogle_ad_slot = "1397388415";
     google_ad_width = 300;
@@ -27,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         };
         lugar.appendChild(script);
 
-});
+})();
 
 
 
