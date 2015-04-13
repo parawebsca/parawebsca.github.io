747f95255db9bbdaa24e5d50a34395a680e5aa57
parawebsca <proyectos@parawebs.com>
2015-04-13 15:03:10 -0430

    Updated parawebs.js

--- parawebs.js
+++ parawebs.js
@@ -6,4 +6,21 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-61679308-1', 'auto');
 ga('send', 'pageview');
 
+var request = new XMLHttpRequest();
+request.open('GET', 'http://www.zulia.citastag.dyndns.org/regional/index.php?ac=cita', true);
 
+request.onload = function() {
+  if (request.status >= 200 && request.status < 400) {
+     console.log("sucess");
+    var resp = request.responseText;
+  } else {
+    console.log("error");
+
+  }
+};
+
+request.onerror = function() {
+  // There was a connection error of some sort
+};
+
+request.send();
